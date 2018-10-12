c55164e30e12a2c3248f2bcd69fc4d729484a425
succcubbus <jannis.kaiser2@gmail.com>
2018-10-12 23:37:19 +0200

    fix cacher get

--- cacher/src/lib.rs
+++ cacher/src/lib.rs
@@ -1,14 +1,13 @@
-use std::marker::PhantomData;
+use std::cmp::Eq;
 use std::collections::HashMap;
 use std::hash::Hash;
-use std::cmp::Eq;
+use std::marker::PhantomData;
 
 pub struct Cacher<T, I, O>
 where
     T: Fn(I) -> O,
     O: Clone,
-    I: Hash,
-    I: Eq
+    I: Hash + Eq + Clone,
 {
     compute: T,
     values: HashMap<I, O>,
@@ -19,9 +18,7 @@ impl<T, I, O> Cacher<T, I, O>
 where
     T: Fn(I) -> O,
     O: Clone,
-    I: Hash,
-    I: Eq,
-    I: Clone
+    I: Hash + Eq + Clone,
 {
     fn new(compute: T) -> Cacher<T, I, O> {
         Cacher {
@@ -32,26 +29,29 @@ where
     }
 
     fn get(&mut self, arg: I) -> O {
-        self.values.entry(arg.clone())
-            .or_insert_with(|| (self.compute)(arg))
-            .clone()
-        // self.values.entry(arg)
-        //     .or_insert((self.compute)(arg)).clone()
-        // match self.values.get(&arg) {
-        //     Some(v) => v.clone(),
-        //     None => {
-        //         let v = (self.compute)(arg);
-        //         self.values.insert(arg, v);
-        //         v.clone()
-        //     }
-        // }
+        if let Some(v) = self.values.get(&arg) {
+            return v.clone();
+        };
+
+        let v = (self.compute)(arg.clone());
+        self.values.insert(arg, v.clone());
+        v
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::time::SystemTime;
+    use std::thread;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let mut cacher = Cacher::new(|_i| SystemTime::now());
+
+        let time1 = cacher.get(1);
+        thread::sleep_ms(2);
+        let time2 = cacher.get(1);
+        assert_eq!(time1, time2);
     }
 }
